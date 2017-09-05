1928380eb6c303d84a6dad026ab131baed55ee40
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2017-09-05 16:46:46 -0700

    add all files to git

---
+++ RANSAC.py
@@ -0,0 +1,67 @@
+# Import PCL module
+import pcl
+
+# Load Point Cloud file
+cloud = pcl.load_XYZRGB('tabletop.pcd')
+
+def voxel_downsampling(pcl_data):
+    # Voxel Grid filtering
+    # Create a VoxelGrid filter object for out input point cloud
+    vox = pcl_data.make_voxel_grid_filter()
+
+    # choose a voxel (leaf) size
+    LEAF_SIZE = 0.01
+
+    # Set voxel size
+    vox.set_leaf_size(LEAF_SIZE, LEAF_SIZE, LEAF_SIZE)
+
+    # call the filter funciton to obtain the resultant downsampled point cloud
+    pcl_filtered = vox.filter()
+    return pcl_filtered
+
+def passthrough_filtering(pcl_data):
+    # PassThrough filtering
+    # Create a PassThrough filter objects
+    passthrough = pcl_data.make_passthrough_filter()
+
+    # Assign axis and range to the passthrough filter objects
+    filter_axis = 'z'
+    passthrough.set_filter_field_name(filter_axis)
+
+    # set the limits
+    axis_min = 0.6  # this retains the table and the objects
+    axis_max = 1.1
+
+    passthrough.set_filter_limits(axis_min, axis_max)
+
+    # Finally, use the filter function to obtain the resultant point cloud
+    pcl_filtered = passthrough.filter()
+    return pcl_filtered
+
+def plane_fitting(pcl_data):
+    # RANSAC plane segmentation
+    # Create the segmentation object
+    seg = pcl_data.make_segmenter()
+
+    # Set the model you wish to filter
+    seg.set_model_type(pcl.SACMODEL_PLANE)
+    seg.set_method_type(pcl.SAC_RANSAC)
+
+    # Max distance for a point to be considered fitting the model
+    max_distance = 0.01 # this leaves only the table
+    seg.set_distance_threshold(max_distance)
+
+    # Call the segment function to obtain set of inlier indices and model coefficients
+    inliers, coefficients = seg.segment()
+
+    return inliers
+
+def extract_inliers(inliers, pcl_data):
+    # Extract inliers
+    extracted_inliers = pcl_data.extract(inliers, negative=False)
+    return extracted_inliers
+
+def extract_outliers(inliers, pcl_data):
+    # Extract outliers
+    extracted_outliers = pcl_data.extract(inliers, negative=True)
+    return extracted_outliers
---
+++ segmentation.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+# Import modules
+from pcl_helper import *
+from RANSAC import *
+
+# TODO: Define functions as required
+
+# Callback function for your Point Cloud Subscriber
+def pcl_callback(pcl_msg):
+
+    # Convert ROS msg to PCL data
+    pcl_data = ros_to_pcl(pcl_msg)
+
+    # Voxel Grid Downsampling
+    pcl_downsampled = voxel_downsampling(pcl_data)
+
+    # PassThrough Filter
+    pcl_passed = passthrough_filtering(pcl_downsampled)
+
+    # RANSAC Plane Segmentation
+    inliers = plane_fitting(pcl_passed)
+
+    # Extract inliers and outliers
+    # inliers: table
+    # outliers: objects
+    cloud_table = extract_inliers(inliers, pcl_passed)
+    cloud_objects = extract_outliers(inliers, pcl_passed)
+
+    # TODO: Euclidean Clustering
+
+    # TODO: Create Cluster-Mask Point Cloud to visualize each cluster separately
+
+    # Convert PCL data to ROS messages
+    ros_cloud_table = pcl_to_ros(cloud_table)
+    ros_cloud_objects = pcl_to_ros(cloud_objects)
+
+    # Publish ROS messages
+    pcl_objects_pub.publish(ros_cloud_objects)
+    pcl_table_pub.publish(ros_cloud_table)
+
+if __name__ == '__main__':
+
+    # ROS node initialization
+    rospy.init_node('clustering', anonymous=True)
+
+    # Create Subscribers
+    pcl_sub = rospy.Subscriber("/sensor_stick/point_cloud", pc2.PointCloud2, pcl_callback, queue_size=1)
+
+    # Create Publishers
+    pcl_objects_pub = rospy.Publisher("/pcl_objects", PointCloud2, queue_size=1)
+    pcl_table_pub = rospy.Publisher("/pcl_table", PointCloud2, queue_size=1)
+
+    # Initialize color_list
+    get_color_list.color_list = []
+
+    # Spin while node is not shutdown
+    while not rospy.is_shutdown():
+        rospy.spin()
